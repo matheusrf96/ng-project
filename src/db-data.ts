54c9b3e9c797b14692d4832dbf21dac75308d97c
Matheus Rodrigues <matheusrf96@gmail.com>
2019-02-04 10:20:40 -0300

    Implementação do *ngIf

--- src/db-data.ts
+++ src/db-data.ts
@@ -2,7 +2,7 @@ export const COURSES: any = [
     {
         id: 1,
         description: "Angular Core Deep Dive",
-        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        // iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
         longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
         category: 'INTERMEDIATE',
         lessonsCount: 10
@@ -75,8 +75,8 @@ export const COURSES: any = [
         iconUrl: "https://s3-us-west-1.amazonaws.com/angular-university/course-images/material_design.png",
         longDescription: "Build Applications with the official Angular Widget Library",
         category: 'ADVANCED'
-    }
-
+    },
+    undefined,
 ];
 
 
